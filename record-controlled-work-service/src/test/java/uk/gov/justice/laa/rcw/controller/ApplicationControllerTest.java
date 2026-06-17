d98beb284dae0e1d0c6fb7f89e563442375123c2
Drew Clayton <drew.clayton@justice.gov.uk>
2026-06-17 14:22:40 +0100

    added id to tests

--- record-controlled-work-service/src/test/java/uk/gov/justice/laa/rcw/controller/ApplicationControllerTest.java
+++ record-controlled-work-service/src/test/java/uk/gov/justice/laa/rcw/controller/ApplicationControllerTest.java
@@ -45,12 +45,14 @@ class ApplicationControllerTest {
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.*", hasSize(2)))
+        .andExpect(jsonPath("$[0].id").value("a1b2c3d4-e5f6-7890-abcd-ef1234567890"))
         .andExpect(jsonPath("$[0].name").value("Random Name"))
         .andExpect(jsonPath("$[0].dob").value("1985-03-14"))
         .andExpect(jsonPath("$[0].address").value("Random Address"))
         .andExpect(jsonPath("$[1].name").value("Other Random Name"))
         .andExpect(jsonPath("$[1].dob").value("1972-11-28"))
-        .andExpect(jsonPath("$[1].address").value("Other Random Address"));
+        .andExpect(jsonPath("$[1].address").value("Other Random Address"))
+        .andExpect(jsonPath("$[1].id").value("b2c3d4e5-f6a7-8901-bcde-f12345678901"));
   }
 
   @Test
